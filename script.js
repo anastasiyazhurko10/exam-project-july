8bcc729818ce1b2951c9742d3ca7543b989ba117
anastasiyazhurko10 <anastasiya.belanovich10@mail.ru>
2025-07-18 00:36:37 +0300

    Исправила ошибку

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ function renderItems(items) {
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
 const nothingFound = document.getElementById('nothing-found');
-const sortSelect = document.getElementById('sort-select');
+const sortSelect = document.getElementById('sortSelect');
 
 let currentItems = [...items];
 
